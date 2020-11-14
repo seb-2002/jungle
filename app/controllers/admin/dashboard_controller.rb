9800d6b2a47f8eba1278077097ba0aab9a28ac03
Sebastian Kann <sebastiankann@gmail.com>
2020-11-14 22:14:44 +0000

    implemented product and category counts in the admin dashboard controller/view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < ApplicationController
-  #http_basic_authenticate_with :name => ENV['ADMIN_USER'], :password => ENV['ADMIN_PASSWORD'] 
-http_basic_authenticate_with :name => 'Jungle', :password => 'book'
+  http_basic_authenticate_with :name => ENV['ADMIN_USER'], :password => ENV['ADMIN_PASSWORD'] 
 
   def show
+    @stats = {product_count: Product.count, category_count: Category.count}
   end
 end
